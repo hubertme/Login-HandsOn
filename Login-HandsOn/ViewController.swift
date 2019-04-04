2392b6a51561c038dc46693bf2e2b04f16df1ff1
Hubert Wang <hubert.tatra@gmail.com>
2019-04-04 12:08:41 +0700

    Initial Commit

---
+++ Login-HandsOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login-HandsOn
+//
+//  Created by Hubert Wang on 04/04/2019.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
