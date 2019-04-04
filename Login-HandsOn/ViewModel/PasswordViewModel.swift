0103f1addf2b458a6b8eced613b4f84923a00b41
Hubert Wang <hubert.tatra@gmail.com>
2019-04-04 12:49:18 +0700

    Add password view model

---
+++ Login-HandsOn/ViewModel/PasswordViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  PasswordViewModel.swift
+//  Login-HandsOn
+//
+//  Created by Hubert Wang on 04/04/2019.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+
+class PasswordViewModel: ValidationProtocol {
+    
+    // MARK: - Attributes
+    var errorMessage: String = "Please enter a valid password"
+    var data: Variable<String> = Variable("")
+    var errorValue: Variable<String?> = Variable(nil)
+    
+    // MARK: - Methods
+    func validateCredentials() -> Bool {
+        guard validateLength(text: data.value, size: (6,15)) else {
+            errorValue.value = errorMessage
+            return false
+        }
+        
+        errorValue.value = nil
+        return true
+    }
+    
+    private func validateLength(text: String, size: (min: Int, max: Int)) -> Bool {
+        return (size.min ... size.max).contains(text.count)
+    }
+}
