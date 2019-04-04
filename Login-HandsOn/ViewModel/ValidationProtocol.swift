a1151ea5964dd4ba364d81d517a628dbf9dc7ca8
Hubert Wang <hubert.tatra@gmail.com>
2019-04-04 12:38:31 +0700

    Add validation protocol

---
+++ Login-HandsOn/ViewModel/ValidationProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  ValidationViewModel.swift
+//  Login-HandsOn
+//
+//  Created by Hubert Wang on 04/04/2019.
+//  Copyright © 2019 Hubert Wang. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol ValidationProtocol {
+    var errorMessage: String { get }
+    
+    // Observables
+    var data: Variable<String> { get set }
+    var errorValues: Variable<String?> { get }
+    
+    // Validation
+    func validateCredentials() -> Bool
+}
